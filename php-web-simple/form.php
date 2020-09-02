3422d62a73062bd457f3d738340edb7c5d1bf449
choron634 <h.syou94@gmail.com>
2020-09-02 15:21:30 +0000

    initial commit

---
+++ php-web-simple/form.php
@@ -0,0 +1,10 @@
+<html>
+<head><title>ようこそ</title></head>
+<body>
+
+<?php
+$input_data = $_POST['name'];
+echo $input_data;?>さんこんにちは。
+<p><input type="button" onclick="location.href='./index.html'" value="戻る"></p>
+</body>
+</html>
